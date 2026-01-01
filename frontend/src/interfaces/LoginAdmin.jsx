925fdc7ae6619c8dfc65904ccaeb90f1afea32b3
ERMR1 <114266959+ermr2@users.noreply.github.com>
2026-01-01 13:44:01 -0600

    cambios
    ver

--- frontend/src/interfaces/LoginAdmin.jsx
+++ frontend/src/interfaces/LoginAdmin.jsx
@@ -1,19 +1,9 @@
 import React, { useState } from "react";
-import {
-  signInWithEmailAndPassword,
-  sendPasswordResetEmail
-} from "firebase/auth";
-import {
-  collection,
-  query,
-  where,
-  getDocs
-} from "firebase/firestore";
-
-import { auth, db } from "../utilidades/firebase";
-import logoAdmin from "../imagenes/LogoAdmin.png";
-import fondoProyecto from "../imagenes/fondo1.jpg";
-import BackButton from "../utilidades/BackButton";
+import { signInWithEmailAndPassword, sendPasswordResetEmail } from "firebase/auth";
+import { auth } from "../utilidades/firebase";
+import logoAdmin from "../imagenes/LogoAdmin.png"; // imagen del logo del proyecto
+import fondoProyecto from "../imagenes/fondo1.jpg"; // imagen de fondo del proyecto
+import BackButton from "../utilidades/BackButton"; // Botón para regresar al menú de roles
 import "../estilos/LoginAdmin.css";
 import { useNavigate } from "react-router-dom";
 
@@ -29,40 +19,9 @@ export default function LoginAdmin() {
     setError("");
 
     try {
-<<<<<<< HEAD
       await signInWithEmailAndPassword(auth, email, password);
-<<<<<<< HEAD
-      alert("Bienvenido Administrador");
-      navigate("/Menuusuario")
-=======
       alert("Bienvenido Administrador!");
       navigate("/check-admin", { state: { nombre: "Administrador" } });
->>>>>>> main
-=======
-      // 1. Autenticación
-      const userCred = await signInWithEmailAndPassword(auth, email, password); // Iniciar sesión
-      const uid = userCred.user.uid; // Obtener UID
-
-      // 2. Buscar si es un usuario normal en Firestore
-      const ref = collection(db, "Usuario");
-      const q = query(ref, where("UID", "==", uid));
-      const snap = await getDocs(q);
-
-      if (!snap.empty) {
-        const data = snap.docs[0].data();
-
-        // 👇 CORRECTO: verificar rol en minúscula
-        if (data.Rol === "user") {
-          setError("Acceso denegado: no tienes permisos de administrador.");
-          return;
-        }
-      }
-
-      // 3. Si NO existe → automáticamente es administrador
-      alert("Bienvenido Administrador!");
-      navigate("/check-admin", { state: { nombre: "Administrador" } });
-
->>>>>>> main
     } catch (err) {
       console.error(err);
       setError("Correo o contraseña incorrectos.");
@@ -71,30 +30,28 @@ export default function LoginAdmin() {
 
   const handlePasswordReset = async () => {
     if (!email) {
-      setResetMessage("Ingresa tu correo para recuperar la contraseña.");
+      setResetMessage("Por favor ingresa tu correo para recuperar la contraseña.");
       return;
     }
 
     try {
       await sendPasswordResetEmail(auth, email);
-      setResetMessage("Se ha enviado un enlace de recuperación.");
+      setResetMessage("Se ha enviado un enlace de recuperación a tu correo.");
     } catch (err) {
       console.error(err);
-      setResetMessage("Error al enviar el correo. Verifica tu correo.");
+      setResetMessage("Error al enviar el correo. Verifica que sea válido.");
     }
   };
 
   return (
-    <div
-      className="login-container"
-      style={{ backgroundImage: `url(${fondoProyecto})` }}
-    >
+    <div className="login-container" style={{ backgroundImage: `url(${fondoProyecto})`, }}>
       <BackButton to="/roles" label="Regresar" />
 
       <div className="login-card">
         <h2>Login Administrador</h2>
 
-        <img src={logoAdmin} alt="Logo Admin" className="logo-admin" />
+        {/* LOGO DEL ADMIN*/}
+        <img src={logoAdmin} alt="Logo de Admin" className="logo-admin" />
 
         <form onSubmit={handleLogin}>
           <input
@@ -102,7 +59,6 @@ export default function LoginAdmin() {
             placeholder="Correo"
             value={email}
             onChange={(e) => setEmail(e.target.value)}
-            autoComplete = "username"
             required
           />
 
@@ -111,13 +67,13 @@ export default function LoginAdmin() {
             placeholder="Contraseña"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
-            autoComplete="current-password"
             required
           />
 
           <button type="submit">Ingresar</button>
         </form>
 
+        {/* Enlace de recuperación */}
         <p className="forgot-password" onClick={handlePasswordReset}>
           ¿Olvidaste tu contraseña?
         </p>
